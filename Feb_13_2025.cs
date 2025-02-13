f43ccaa7c76fd34289b9bde37af58900de499103
GurmeetKT <GurmeetK695@stemcollegiate.ca>
2025-02-13 10:42:34 -0700

    Feb_13_2025.cs

---
+++ Feb_13_2025.cs
@@ -0,0 +1,4 @@
+//Today I learnt how to write how to write lines in cs.
+Console.WriteLine("This is the first line");
+Console.WriteLine("This is the second line");
+//This is how you write the lines :)
